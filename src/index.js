133420d37a14cd7c98a6f3d76f7b9026995074c3
Ilkin <feyzullayevilkin10@gmail.com>
2025-11-19 22:16:21 +0300

    Add base conversion tool with UI and build setup
    Introduces a base conversion tool with a responsive user interface. Implements number input validation, base selection, and conversion logic. Includes styles for various viewport sizes.
    
    Adds build setup using Webpack for production builds and dependency management. Establishes folder structure for source and build files with appropriate asset handling.
    
    Provides a user-friendly design and clipboard copy functionality for converted results. Optimizes the application for cross-platform usability and maintainability.

---
+++ src/index.js
@@ -0,0 +1,94 @@
+// Initialize
+import { BaseConverter } from "@sihuaguofen/base-converter";
+
+const baseConverter = new BaseConverter();
+
+// Variables
+const inputBase = document.getElementById("inputBase");
+const outputBase = document.getElementById("outputBase");
+
+let regExInput = /^-?(?:0|[1-9][0-9]*)(?:\.[0-9]+)?$/;
+
+const numberInput = document.getElementById("numberInput");
+const numberOutput = document.getElementById("numberOutput");
+
+let output = "";
+
+const copyBtn = document.getElementById("copyBtn");
+const copyMessage = document.getElementById("copyMessage");
+
+// Events
+inputBase.addEventListener("change", function (e) {
+  if (e.target.value == 2) {
+    numberInput.value = "";
+    numberOutput.value = "";
+    regExInput = /[01]/;
+  }
+  if (e.target.value == 8) {
+    numberInput.value = "";
+    numberOutput.value = "";
+    regExInput = /[0-7]/;
+  }
+  if (e.target.value == 10) {
+    numberInput.value = "";
+    numberOutput.value = "";
+    regExInput = /[0-9]/;
+  }
+  if (e.target.value == 16) {
+    numberInput.value = "";
+    numberOutput.value = "";
+    regExInput = /[0-9A-Fa-f]/;
+  }
+});
+
+outputBase.addEventListener("change", function () {
+  if (numberInput.value !== "") {
+    numberOutput.value = calcOutput();
+  } else {
+    numberOutput.value = "";
+  }
+});
+
+numberInput.addEventListener("keydown", function (e) {
+  const valid = regExInput.test(e.key);
+  if (e.key != "Backspace" && !valid) {
+    e.preventDefault();
+  }
+});
+
+numberInput.addEventListener("keyup", function () {
+  if (numberInput.value !== "") {
+    numberOutput.value = calcOutput();
+  } else {
+    numberOutput.value = "";
+  }
+});
+
+document.addEventListener("click", function () {
+  console.log({
+    numberInput: numberInput.value,
+    numberOutput: numberOutput.value,
+    inputBase: inputBase.value,
+    outputBase: outputBase.value,
+    output,
+  });
+});
+
+copyBtn.addEventListener("click", function () {
+  navigator.clipboard.writeText(numberOutput.value);
+
+  copyMessage.textContent = "Copied!";
+
+  setTimeout(() => {
+    copyMessage.textContent = "Copy";
+  }, 1000);
+});
+
+// Functions
+function calcOutput() {
+  output = baseConverter.convert(numberInput.value.toUpperCase(), {
+    fromBase: Number(inputBase.value),
+    toBase: Number(outputBase.value),
+  });
+  return output;
+}
